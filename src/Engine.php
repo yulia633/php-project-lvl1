34098ccc42ed9d245fa031a873ef5fa0137cd9b7
yulia633 <ignat633yulia@yandex.ru>
2020-10-22 22:23:31 +0300

    refactor Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,30 +5,30 @@ namespace Brain\Engine;
 use function cli\line;
 use function cli\prompt;
 
-const CORRECT_ANSWER = 3;
+const ROUND = 3;
 
-function gameLogic($gameConditionsEven, $generateGameData)
+function gameLogic($description, $generateGameData)
 {
     line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, {$name}!");
-    line($gameConditionsEven);
+    line($description);
 
-    for ($i = 1; $i <= CORRECT_ANSWER; $i += 1) {
+    for ($i = 1; $i <= ROUND; $i += 1) {
         [$question, $answer] = $generateGameData();
 
         line("Question: {$question}");
-        $currentAnswer = prompt("Your answer");
+        $playerAnswer = prompt("Your answer");
 
-        if ($currentAnswer === $answer) {
+        if ($playerAnswer === $answer) {
             line("Correct!");
             continue;
         } else {
-            line("'{$currentAnswer}' is wrong answer ;(. Correct answer was '{$answer}'");
+            line("'{$playerAnswer}' is wrong answer ;(. Correct answer was '{$answer}'");
             line("Let`s try again, {$name}!");
             return;
         }
     }
 
-    return line("Congratulations, {$name}!");
+    line("Congratulations, {$name}!");
 }
